105fb1b78de8156f6074adb47e18af9b20dcdb5c
Guilherme Silvestre <gaasilvestre@gmail.com>
2023-05-23 13:46:54 -0300

    Começando React

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './style.css';
+
+function App() {
+  return (
+    <header class="header">
+      <div class="logo">
+        <img src="logo.png" alt="Today I Learned" />
+        <h1>Today I learned</h1>
+      </div>
+
+      <button class="btn btn-large btn-open">Share a fact</button>
+    </header>
+  );
+}
+
+export default App;
