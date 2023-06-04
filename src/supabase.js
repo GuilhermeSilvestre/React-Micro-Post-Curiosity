12fc39fee72acf2aa3ad9fc458f401821a25bbce
Guilherme Silvestre <100291684+GuilhermeSilvestre@users.noreply.github.com>
2023-06-04 16:42:57 -0300

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://uznrpckxphngfepybcyd.supabase.co';
-const supabaseKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV6bnJwY2t4cGhuZ2ZlcHliY3lkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQ4MDU4NjksImV4cCI6MjAwMDM4MTg2OX0.WsbITBdpI5CadJDT5Qqmmuj9hIMZEKJfeLvch48wFxk';
+const supabaseUrl = '';
+const supabaseKey = '';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
